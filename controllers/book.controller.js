af5b7111d7b0eaafaa29d9db3f4be375f45e9c11
Sarthak Verma <sarthakverma48486@gmail.com>
2024-07-26 02:15:08 +0530

    testing

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -2,70 +2,70 @@ const postgre = require('../database')
 const bookController = {
     getAll: async(req, res) => {
         try {
-            const { rows } = await postgre.query("select * from books")
+            const { rows } = await postgre.query("select * from employees")
             res.json({msg: "OK", data: rows})
         } catch (error) {
             res.json({msg: error.msg})
         }
-    },
-    getById: async(req, res) => {
-        try {
-            const { rows } = await postgre.query("select * from books where book_id = $1", [req.params.id])
+    }
+    // getById: async(req, res) => {
+    //     try {
+    //         const { rows } = await postgre.query("select * from books where book_id = $1", [req.params.id])
 
-            if (rows[0]) {
-                return res.json({msg: "OK", data: rows})
-            }
+    //         if (rows[0]) {
+    //             return res.json({msg: "OK", data: rows})
+    //         }
 
-            res.status(404).json({msg: "not found"})
-        } catch (error) {
-            res.json({msg: error.msg})
-        }
-    },
-    create: async(req, res) => {
-        try {
-            const { name, price } = req.body
+    //         res.status(404).json({msg: "not found"})
+    //     } catch (error) {
+    //         res.json({msg: error.msg})
+    //     }
+    // },
+    // create: async(req, res) => {
+    //     try {
+    //         const { name, price } = req.body
 
-            const sql = 'INSERT INTO books(name, price) VALUES($1, $2) RETURNING *'
+    //         const sql = 'INSERT INTO books(name, price) VALUES($1, $2) RETURNING *'
 
-            const { rows } = await postgre.query(sql, [name, price])
+    //         const { rows } = await postgre.query(sql, [name, price])
 
-            res.json({msg: "OK", data: rows[0]})
+    //         res.json({msg: "OK", data: rows[0]})
 
-        } catch (error) {
-            res.json({msg: error.msg})
-        }
-    },
-    updateById: async(req, res) => {
-        try {
-            const { name, price } = req.body
+    //     } catch (error) {
+    //         res.json({msg: error.msg})
+    //     }
+    // },
+    // updateById: async(req, res) => {
+    //     try {
+    //         const { name, price } = req.body
 
-            const sql = 'UPDATE books set name = $1, price = $2 where book_id = $3 RETURNING *'
+    //         const sql = 'UPDATE books set name = $1, price = $2 where book_id = $3 RETURNING *'
 
-            const { rows } = await postgre.query(sql, [name, price, req.params.id])
+    //         const { rows } = await postgre.query(sql, [name, price, req.params.id])
 
-            res.json({msg: "OK", data: rows[0]})
+    //         res.json({msg: "OK", data: rows[0]})
 
-        } catch (error) {
-            res.json({msg: error.msg})
-        }
-    },
-    deleteById: async(req, res) => {
-        try {
-            const sql = 'DELETE FROM books where book_id = $1 RETURNING *'
+    //     } catch (error) {
+    //         res.json({msg: error.msg})
+    //     }
+    // },
+    // deleteById: async(req, res) => {
+    //     try {
+    //         const sql = 'DELETE FROM books where book_id = $1 RETURNING *'
 
-            const { rows } = await postgre.query(sql, [req.params.id])
+    //         const { rows } = await postgre.query(sql, [req.params.id])
 
-            if (rows[0]) {
-                return res.json({msg: "OK", data: rows[0]})
-            }
+    //         if (rows[0]) {
+    //             return res.json({msg: "OK", data: rows[0]})
+    //         }
 
-            return res.status(404).json({msg: "not found"})
+    //         return res.status(404).json({msg: "not found"})
             
 
-        } catch (error) {
-            res.json({msg: error.msg})
-        }
-    }
+    //     } catch (error) {
+    //         res.json({msg: error.msg})
+    //     }
+    // }
 }
 
 module.exports = bookController
