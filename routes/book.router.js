7575c4268e242e94f1e0418681ab126001fe3da9
Sarthak Verma <sarthakverma48486@gmail.com>
2024-07-29 20:34:57 +0530

    login api

--- routes/book.router.js
+++ routes/book.router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const bookController = require('../controllers/book.controller')
 
 router.get("/", bookController.getAll)
-router.get("login/:email/:password", bookController.loginByEmailPassword)
+router.get("/login/:email/:password", bookController.loginByEmailPassword)
 router.post("/buyOptions", bookController.buyCreateTable)
 router.get("/buyOptions/:userId", bookController.getBuyRecordsById)
 // router.put("/:id", bookController.updateById)
